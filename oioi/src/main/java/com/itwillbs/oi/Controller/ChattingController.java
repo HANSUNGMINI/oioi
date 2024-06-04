9aaee95abf9d39d7c82cbe2c6566d1ea228592d8
배소은 <0b506h@gmail.com>
2024-06-04 16:02:36 +0900

    배소은 / 채팅 컨트롤러

---
+++ oioi/src/main/java/com/itwillbs/oi/Controller/ChattingController.java
@@ -0,0 +1,15 @@
+package com.itwillbs.oi.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChattingController {
+
+	@GetMapping("ChatList")
+	public String goChatList() {
+		return "chatting/chatting_list";
+	}
+	
+	
+}
