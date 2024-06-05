6d270243cebd561aae00f41e9d4b77d51e1cc1d6
김유신 <dbtls101965@gmail.com>
2024-06-05 11:31:39 +0900

    김유신 / 거래탭 합치기

--- oioi/src/main/java/com/itwillbs/oi/Controller/TradeController.java
+++ oioi/src/main/java/com/itwillbs/oi/Controller/TradeController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SellingController {
+public class TradeController {
 	
-	@GetMapping("selling")
-	public String Auction() {
-		return "selling";
+	
+	@GetMapping("trade")
+	public String goTrade() {
+		return "trade";
 	}
 }
