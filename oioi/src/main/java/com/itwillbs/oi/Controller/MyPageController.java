afba323ab7fb1cbe91596efd54a47fe1c1e1777f
허정민 <manbul97@naver.com>
2024-06-13 17:38:09 +0900

    허정민 - myPage (11.89%)

--- oioi/src/main/java/com/itwillbs/oi/Controller/MyPageController.java
+++ oioi/src/main/java/com/itwillbs/oi/Controller/MyPageController.java
@@ -1,13 +1,24 @@
 package com.itwillbs.oi.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPageController {
 	
 	@GetMapping("myPage")
-	public String myPage() {
+	public String myPage(HttpSession session, Model model) {
+		
+		
 		return "mypage/mypage";
+	}
+	
+	@GetMapping("user")
+	public String user() {
+		System.out.println("확인");
+		return "mypage/mypage_user";
 	}
 }
