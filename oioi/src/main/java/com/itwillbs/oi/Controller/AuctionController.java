d7c1da5058fd4c45e93276d05b299495f57072f6
한성민 <gkstyd1313@naver.com>
2024-06-13 18:34:27 +0900

    한성민 - 경매 상품 카테고리

--- oioi/src/main/java/com/itwillbs/oi/Controller/AuctionController.java
+++ oioi/src/main/java/com/itwillbs/oi/Controller/AuctionController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.itwillbs.oi.service.AuctionService;
 
 @Controller
@@ -30,11 +32,36 @@ public class AuctionController {
 	@GetMapping("auctionRegist")
 	public String auctionRegist(Model model) {
 		System.out.println("auctionRegist - controller");
-		List<Map<String, String>> cate1 = service.getCategory1();
 		
+		//대분류
+		List<Map<String, String>> cate1 = service.getCategory1();
 		model.addAttribute("cate1", cate1);
 		
+		//중분류
+		List<Map<String, String>> cate2 = service.getCategory2();
+		System.out.println("cate2 : " + cate2);
+		JsonArray jCate2 = new JsonArray();
+		for(Map<String, String> c2 : cate2) {
+			JsonObject jo = new JsonObject();
+			jo.addProperty("CTG_CODE", c2.get("CTG_CODE"));
+			jo.addProperty("CTG_NAME", c2.get("CTG_NAME"));
+			jo.addProperty("UP_CTG_CODE", c2.get("UP_CTG_CODE"));
+			jCate2.add(jo);
+		}
+		model.addAttribute("cate2", jCate2);
 		
+		//소분류
+		List<Map<String, String>> cate3 = service.getCategory3();
+		System.out.println("cate3 : " + cate3);
+		JsonArray jCate3 = new JsonArray();
+		for(Map<String, String> c3 : cate3) {
+			JsonObject jo3 = new JsonObject();
+			jo3.addProperty("CTG_CODE", c3.get("CTG_CODE"));
+			jo3.addProperty("CTG_NAME", c3.get("CTG_NAME"));
+			jo3.addProperty("UP_CTG_CODE", c3.get("UP_CTG_CODE"));
+			jCate3.add(jo3);
+		}
+		model.addAttribute("cate3", jCate3);
 		return "auction/auction_regist";
 	}
 	
