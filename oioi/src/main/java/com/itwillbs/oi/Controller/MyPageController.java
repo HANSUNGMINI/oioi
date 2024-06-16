4761f5e0861a39a2c360980039660e44d42e0798
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-06-17 08:58:44 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/HANSUNGMINI/oioi.git into main

--- oioi/src/main/java/com/itwillbs/oi/Controller/MyPageController.java
+++ oioi/src/main/java/com/itwillbs/oi/Controller/MyPageController.java
@@ -1,17 +1,13 @@
 package com.itwillbs.oi.Controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPageController {
 	
 	@GetMapping("myPage")
-	public String myPage(HttpSession session, Model model) {
-		
+	public String myPage() {
 		
 		return "mypage/mypage";
 	}
